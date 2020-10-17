1c906d049117167c94b74c6bacc3fd03a905133e
Vladislav Khlebec <vladislavkhlebec@MacBook-Pro-Vladislav.local>
2020-10-17 19:15:28 +0500

    feat: initial commit

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+
+const HomeScreen = () => {
+  return <Text style={styles.text}>Hi there!</Text>;
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30,
+    color: 'green'
+  }
+});
+
+export default HomeScreen;
---
+++ src/screens/ListScreen.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+
+const friends = [
+  {
+    name: "Friend #1",
+  },
+  {
+    name: "Friend #2",
+  },
+  {
+    name: "Friend #3",
+  },
+  {
+    name: "Friend #4",
+  },
+  {
+    name: "Friend #5",
+  },
+  {
+    name: "Friend #6",
+  },
+  {
+    name: "Friend #7",
+  },
+  {
+    name: "Friend #8",
+  },
+  {
+    name: "Friend #9",
+  },
+];
+
+const ListScreen = () => {
+  return (
+    <FlatList
+      keyExtractor={(friend) => friend.name}
+      data={friends}
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      renderItem={({ item }) => {
+        return <Text style={styles.textStyle}>{item.name}</Text>;
+      }}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+    textStyle: {
+        marginVertical: 50,
+    }
+});
+
+export default ListScreen;
